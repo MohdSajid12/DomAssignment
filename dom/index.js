939c36974bf1e387b60b6e21288c1cc3cab3675e
MohdSajid12 <108975916+MohdSajid12@users.noreply.github.com>
2022-07-10 21:37:02 +0530

    Update 3rd element and element background

--- dom/index.js
+++ dom/index.js
@@ -3,3 +3,16 @@ console.log(document.all[10]);
 var headerTitle = document.getElementById('header-title');
 
 headerTitle.style.borderBottom= 'solid 3px #0000';
+
+
+
+var items = document.getElementsByClassName('list-group-item');
+
+items[3].style.backgroundColor ='green';
+
+
+
+for(var i=0 ;i<items.length;i++)
+{
+    items[i].style.fontWeight = 'bold';
+}
