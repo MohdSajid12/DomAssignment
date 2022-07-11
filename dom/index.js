e2195da439183d9d56ae2d4406589bda71c87402
MohdSajid12 <108975916+MohdSajid12@users.noreply.github.com>
2022-07-11 14:12:19 +0530

    use of  querySelector and querySelectorAll

--- dom/index.js
+++ dom/index.js
@@ -16,3 +16,22 @@ for(var i=0 ;i<items.length;i++)
 {
     items[i].style.fontWeight = 'bold';
 }
+
+
+var secondItem = document.querySelector('.list-group');
+
+ secondItem.children[2].style.backgroundColor='green';
+
+ secondItem.children[3].style.display ='none';
+
+
+
+ var ite = document.querySelectorAll('li')
+ ite[2].style.color = 'green';
+
+
+var odd = document.querySelectorAll('li :nth-child(odd)');
+for(var i=0; i<odd.lenngth; i++)
+{
+    odd[i].style.backgroundColor = 'green';
+}
