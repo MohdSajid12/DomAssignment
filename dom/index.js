1618ec9ece6274b7753d50e7b12f1d6258f9a01f
MohdSajid12 <108975916+MohdSajid12@users.noreply.github.com>
2022-07-11 15:27:49 +0530

    Traversal Dom

--- dom/index.js
+++ dom/index.js
@@ -35,3 +35,49 @@ for(var i=0; i<odd.lenngth; i++)
 {
     odd[i].style.backgroundColor = 'green';
 }
+
+
+var itemList = document.querySelector('#items')
+console.log(itemList.parentElement);
+
+console.log(itemList.lastElementChild);
+
+console.log(itemList.lastChild);
+
+console.log(itemList.firstElementChild);
+
+console.log(itemList.firstChild);
+
+console.log(itemList.nextSibling);
+
+console.log(itemList.nextElementSibling);
+
+console.log(itemList.previousSibling);
+
+console.log(itemList.previousElementSibling);
+
+
+var newDiv = document.createElement('div');
+
+newDiv.className = 'hello';
+
+newDiv.id = 'hello2';
+
+newDiv.setAttribute('titile' , 'hello div');
+
+var newDivText = document.createTextNode("hello boy")
+newDiv.appendChild(newDivText);
+
+
+var container = document.querySelector('header .container')
+var h1 = document.querySelector('header h1');
+
+console.log(newDiv)
+container.insertBefore(newDiv , h1);
+
+
+
+var parentnode = document.getElementById('.list-group');
+console.log(parentnode.innerHTML);
+
+parentnode = '<li> hello </li>'+parentnode;
